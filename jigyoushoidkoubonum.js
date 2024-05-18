1426ff9accc742bba00a9b9cfacfea2614318669
wtnbyshr <91499808+wtnbyshr@users.noreply.github.com>
2024-05-18 18:36:46 +0900

    Update jigyoushoidkoubonum.js

--- jigyoushoidkoubonum.js
+++ jigyoushoidkoubonum.js
@@ -3,10 +3,10 @@
 
   fb.events.form.created = [function (state) {
     console.log(state);
-      const date = luxon.DateTime.local();
+      
       const jigyoushaid = state.record.事業者ID.value
     
-    state.record.string1.value = 'edit string data';
+    state.record.媒介公募番号事業者ID.value = baikainum + jigyoushaid
 
     return state;  // 変更後の状態を返さない場合、一部の変更が反映されない場合があります
   }];
