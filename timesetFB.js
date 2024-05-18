704265c03d8fecea633d4319dab8de790d9b1c31
wtnbyshr <91499808+wtnbyshr@users.noreply.github.com>
2024-05-18 13:21:54 +0900

    Update timesetFB.js

--- timesetFB.js
+++ timesetFB.js
@@ -5,7 +5,7 @@
     const my_field_code = 'Now_datetime';
   
     // 新規登録と編集画面でイベント発火
-    fb.fb.events.form.confirm = [function (state) {
+    fb.events.form.confirm = [function (state) {
 
         // 現在の日時を取得
         const currentDatetime = new Date().toISOString();
