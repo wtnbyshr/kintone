a18bddfcfb351f104194defc292413067b8e74fa
wtnbyshr <91499808+wtnbyshr@users.noreply.github.com>
2024-05-18 18:14:08 +0900

    Update timesetFB.js

--- timesetFB.js
+++ timesetFB.js
@@ -13,6 +13,18 @@
         const currentDatetime = date.toISO();
             
         // フィールドに値を設定
+        (function() {
+        var textField1 = fb.getElementByCode('媒介公募番号');
+        var textField2 = fb.getElementByCode('事業者ID');
+        console.log(textField1 & textField2);
+
+        // 取得した要素に初期値を設定します
+        textField.getElementsByTagName('媒介公募番号事業者ID')[0].value = textField1 & textField2;
+
+    return state;
+  }];
+})();
+      
         state.record.my_field_code.value = currentDatetime;
 
       return state; 
