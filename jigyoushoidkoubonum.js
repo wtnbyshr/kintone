dce33a521ebeb9f5412cddc4bd841b7705dabbba
wtnbyshr <91499808+wtnbyshr@users.noreply.github.com>
2024-05-18 18:51:08 +0900

    Update jigyoushoidkoubonum.js

--- jigyoushoidkoubonum.js
+++ jigyoushoidkoubonum.js
@@ -6,7 +6,7 @@
       const baikainum = state.record.媒介公募番号.value
       const jigyoushaid = state.record.事業者ID.value
     
-    state.record.媒介公募番号事業者ID.value = baikainum + jigyoushaid
+    state.record.jigyoushakouboid.value = baikainum + jigyoushaid
 
     return state;  // 変更後の状態を返さない場合、一部の変更が反映されない場合があります
   }];
