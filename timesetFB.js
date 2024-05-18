22e77ff3595bd5f1c89a211f06e0da2611242d87
wtnbyshr <91499808+wtnbyshr@users.noreply.github.com>
2024-05-18 13:19:47 +0900

    Create timesetFB.js

---
+++ timesetFB.js
@@ -0,0 +1,18 @@
+/* 現在日時入力ボタン */
+(function() {
+    'use strict';
+    // 初期設定 フィールドコードDの設定
+    const my_field_code = 'Now_datetime';
+  
+    // 新規登録と編集画面でイベント発火
+    fb.fb.events.form.confirm = [function (state) {
+
+        // 現在の日時を取得
+        const currentDatetime = new Date().toISOString();
+            
+        // フィールドに値を設定
+        textField.getElementByCode(my_field_code).value = currentDatetime;
+       
+
+   }];
+})();
