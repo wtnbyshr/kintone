74cb067cbf47c90f6a6266ae5a661ad27011b706
wtnbyshr <91499808+wtnbyshr@users.noreply.github.com>
2024-05-18 20:01:07 +0900

    Update timesetFB.js

--- timesetFB.js
+++ timesetFB.js
@@ -8,7 +8,7 @@
   fb.events.form.mounted = [function (state) {
     // 現在の日時を取得しISOを取得
     const currentDatetime = date.toISO();
-    const currentDatetimemill = date.millisecond;
+    const currentDatetimemill = luxon.DateTime.fromISO('2023-10-01T00:00:00.000+09:00').toFormat('SSS');;
 
     // フィールドに値を設定
     state.record.Now_datetime.value = currentDatetime;
