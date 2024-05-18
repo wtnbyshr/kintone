391c4f5dd357d3bae9606e61477408cedc0f31bb
wtnbyshr <91499808+wtnbyshr@users.noreply.github.com>
2024-05-18 18:36:11 +0900

    Update jigyoushoidkoubonum.js

--- jigyoushoidkoubonum.js
+++ jigyoushoidkoubonum.js
@@ -3,8 +3,9 @@
 
   fb.events.form.created = [function (state) {
     console.log(state);
-    state.form.title = 'edit form title';
-    state.fields[0].hide = true;
+      const date = luxon.DateTime.local();
+      const jigyoushaid = state.record.事業者ID.value
+    
     state.record.string1.value = 'edit string data';
 
     return state;  // 変更後の状態を返さない場合、一部の変更が反映されない場合があります
