4abe35b4d79a6dd88792a08befd18b5be087ba5d
wtnbyshr <91499808+wtnbyshr@users.noreply.github.com>
2024-05-18 19:55:24 +0900

    Update timesetFB.js

--- timesetFB.js
+++ timesetFB.js
@@ -8,7 +8,7 @@
   fb.events.form.mounted = [function (state) {
     // 現在の日時を取得しISOを取得
     const currentDatetime = date.toISO();
-    const currentDatetimemill = date.fromISO('SSS').toISO() ;
+    const currentDatetimemill = date.millisecond;
 
     // フィールドに値を設定
     state.record.Now_datetime.value = currentDatetime;
